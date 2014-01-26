0ef1f4b5dc59537bced558ff2bdf8cc51be5176b
r3d32 <cagudo@lion.lmu.edu>
2014-01-26 01:59:04 -0800

    Implemented Character movement

---
+++ gameDirectory/gameProject/Assets/Scripts/LinkController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LinkController : MonoBehaviour {
+	public float maxSpeed = 10f;
+	bool facingRight = true;
+
+	public Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>(); 
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		anim = GetComponent<Animator>(); 
+		float move = Input.GetAxis ("Horizontal");
+		anim.SetFloat ( "Speed", Mathf.Abs(move));
+		float moveY = Input.GetAxis ("Vertical");
+		anim.SetFloat ( "SpeedY", moveY );
+		rigidbody2D.velocity = new Vector2 (move * maxSpeed, rigidbody2D.velocity.y);
+		rigidbody2D.velocity = new Vector3 (rigidbody2D.velocity.x, moveY * maxSpeed);
+
+		if (move > 0 &&!facingRight) 
+			Flip ();
+		else if (move < 0 && facingRight)
+			Flip ();
+	}
+
+	void Flip() {
+
+		facingRight = !facingRight;
+		Vector4 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+
+	}
+}
+
