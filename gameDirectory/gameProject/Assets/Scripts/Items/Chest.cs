dcaaa02a2669084881d610e7f8269955e3144c05
r3d32 <cagudo@lion.lmu.edu>
2014-02-23 18:27:35 -0800

    Implemented chest object that releases items after opened

---
+++ gameDirectory/gameProject/Assets/Scripts/Items/Chest.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Chest : MonoBehaviour {
+
+	/********** Chest Variables **********/
+	public Animator chestAnimator;
+	public GameObject[] itemsHeld;
+	
+	/********** METHOD TO OPEN CHEST **********/
+	void OnTriggerEnter2D (Collider2D other){
+		if (other.gameObject.tag == "character" && Input.GetKeyDown (KeyCode.E)) {
+			chestAnimator = GetComponent<Animator>(); 
+			chestAnimator.SetBool("closed", false);
+			ReleaseItems();
+		}
+	}
+	void ReleaseItems(){
+		foreach (GameObject item in itemsHeld) {
+			//item.collider2D.enabled
+			item.GetComponent<SpriteRenderer>().enabled = true;
+			item.GetComponent<Collider2D>().enabled = true;
+		}
+	}
+
+	void Update () {
+	
+	}
+}
